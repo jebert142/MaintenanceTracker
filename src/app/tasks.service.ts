79f15af78ff4ed5f92d6c9612690f3b6c3e7207a
Jon Ebert <jebert142@gmail.com>
2022-02-06 11:13:51 -0500

    Adding the 'Task' Service

---
+++ src/app/tasks.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+// import { MaintenanceItem } from './models/maintenanceItem';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class TasksService {
+
+  constructor() { }
+
+
+  // getTasks(): MaintenanceItem[] {
+  //   return MaintenanceItem;
+  // }
+}
